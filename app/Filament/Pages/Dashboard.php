2d16faf025e8c73b54e9c21d6b25170ff854dc1b
EL OUFIR Hatim <eloufirhatim@gmail.com>
2022-11-08 15:31:55 +0100

    Dashboard override class enhancement

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -6,6 +6,8 @@ use Filament\Pages\Dashboard as BasePage;
 
 class Dashboard extends BasePage
 {
+    protected static bool $shouldRegisterNavigation = false;
+
     protected function getColumns(): int | array
     {
         return 6;
