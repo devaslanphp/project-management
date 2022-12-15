cc26e2ac4e789db89037a032ea8b8e9d7e1f4a87
EL OUFIR Hatim <eloufirhatim@gmail.com>
2022-12-15 14:31:55 +0100

    Add non epic tickets to road map

--- app/Http/Controllers/RoadMap/DataController.php
+++ app/Http/Controllers/RoadMap/DataController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\RoadMap;
 use App\Http\Controllers\Controller;
 use App\Models\Epic;
 use App\Models\Project;
+use App\Models\Ticket;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Collection;
 
@@ -20,7 +21,7 @@ class DataController extends Controller
     public function data(Project $project): JsonResponse
     {
         $epics = Epic::where('project_id', $project->id)->get();
-        return response()->json($this->formatResponse($epics));
+        return response()->json($this->formatResponse($epics, $project));
     }
 
     /**
@@ -29,63 +30,90 @@ class DataController extends Controller
      * @param Collection $epics
      * @return Collection
      */
-    private function formatResponse(Collection $epics): Collection
+    private function formatResponse(Collection $epics, Project $project): Collection
     {
         $results = collect();
         foreach ($epics as $epic) {
-            $results->push(collect([
-                "pID" => $epic->id,
-                "pName" => $epic->name,
-                "pStart" => $epic->starts_at->format('Y-m-d'),
-                "pEnd" => $epic->ends_at->format('Y-m-d') . " 23:59:59",
-                "pClass" => "g-custom-task",
-                "pLink" => "",
-                "pMile" => 0,
-                "pRes" => "",
-                "pComp" => "",
-                "pGroup" => 1,
-                "pParent" => 0,
-                "pOpen" => 1,
-                "pDepend" => "",
-                "pCaption" => "",
-                "pNotes" => "",
-                "pBarText" => "",
-                "meta" => [
-                    "id" => $epic->id,
-                    "epic" => true,
-                    "parent" => null,
-                    "slug" => null
-                ]
-            ]));
+            $results->push(collect($this->epicObj($epic)));
             foreach ($epic->tickets as $ticket) {
-                $pComp = round($ticket->completudePercentage, 0);
-                $results->push(collect([
-                    "pID" => $epic->id . $ticket->id,
-                    "pName" => $ticket->name,
-                    "pStart" => "",
-                    "pEnd" => "",
-                    "pClass" => "g-custom-task",
-                    "pLink" => "",
-                    "pMile" => 0,
-                    "pRes" => $ticket->responsible?->name ?? "",
-                    "pComp" => min($pComp, 100),
-                    "pGroup" => 0,
-                    "pParent" => $epic->id,
-                    "pOpen" => 1,
-                    "pDepend" => "",
-                    "pCaption" => "",
-                    "pNotes" => "",
-                    "pBarText" => "",
-                    "meta" => [
-                        "id" => $ticket->id,
-                        "epic" => false,
-                        "parent" => $epic->id,
-                        "slug" => $ticket->code
-                    ]
-                ]));
+                $results->push(collect($this->ticketObj($epic, $ticket)));
             }
         }
+        $tickets = Ticket::where('project_id', $project->id)->whereNull('epic_id')->get();
+        foreach ($tickets as $ticket) {
+            $results->push(collect($this->ticketObj(null, $ticket)));
+        }
         return $results;
+    }
+
+    /**
+     * Format Epic object
+     *
+     * @param Epic $epic
+     * @return array
+     */
+    private function epicObj(Epic $epic)
+    {
+        return [
+            "pID" => $epic->id,
+            "pName" => $epic->name,
+            "pStart" => $epic->starts_at->format('Y-m-d'),
+            "pEnd" => $epic->ends_at->format('Y-m-d') . " 23:59:59",
+            "pClass" => "g-custom-task",
+            "pLink" => "",
+            "pMile" => 0,
+            "pRes" => "",
+            "pComp" => "",
+            "pGroup" => 1,
+            "pParent" => 0,
+            "pOpen" => 1,
+            "pDepend" => "",
+            "pCaption" => "",
+            "pNotes" => "",
+            "pBarText" => "",
+            "meta" => [
+                "id" => $epic->id,
+                "epic" => true,
+                "parent" => null,
+                "slug" => null
+            ]
+        ];
+    }
+
+    /**
+     * Format Ticket object
+     *
+     * @param Epic $epic
+     * @param Ticket $ticket
+     * @return array
+     */
+    private function ticketObj(Epic|null $epic, Ticket $ticket)
+    {
+        $pComp = round($ticket->completudePercentage, 0);
+        return [
+            "pID" => ($epic?->id ?? "N") . $ticket->id,
+            "pName" => $ticket->name,
+            "pStart" => "",
+            "pEnd" => "",
+            "pClass" => "g-custom-task",
+            "pLink" => "",
+            "pMile" => 0,
+            "pRes" => $ticket->responsible?->name ?? "",
+            "pComp" => min($pComp, 100),
+            "pGroup" => 0,
+            "pParent" => $epic?->id ?? "",
+            "pOpen" => 1,
+            "pDepend" => "",
+            "pCaption" => "",
+            "pNotes" => "",
+            "pBarText" => "",
+            "meta" => [
+                "id" => $ticket->id,
+                "epic" => false,
+                "parent" => $epic?->id ?? null,
+                "slug" => $ticket->code
+            ]
+        ];
     }
 
 }
