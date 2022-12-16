52be15a8d8f436d3e0b6d6f989d357cf8b014005
EL OUFIR Hatim <eloufirhatim@gmail.com>
2022-12-16 14:34:16 +0100

    Add issue creation (dialog) into kanban view

--- resources/views/partials/kanban/status.blade.php
+++ resources/views/partials/kanban/status.blade.php
@@ -14,12 +14,27 @@
             @include('partials.kanban.record')
         @endforeach
 
+        <!-- href="{{ route('filament.resources.tickets.create', ['project' => request()->get('project')]) }}" -->
         @if($status['add_ticket'])
-            <a class="create-record"
-               href="{{ route('filament.resources.tickets.create', ['project' => request()->get('project')]) }}"
+            <a class="create-record hover:cursor-pointer"
+               wire:click="createTicket"
                target="_blank">
                 <x-heroicon-o-plus class="w-4 h-4" /> {{ __('Create ticket') }}
             </a>
+
+            @if($ticket)
+                <!-- Epic modal -->
+                <div class="dialog-container">
+                    <div class="dialog dialog-xl">
+                        <div class="dialog-header">
+                            {{ __('Create ticket') }}
+                        </div>
+                        <div class="dialog-content">
+                            @livewire('road-map.issue-form', ['project' => null])
+                        </div>
+                    </div>
+                </div>
+            @endif
         @endif
     </div>
 </div>
