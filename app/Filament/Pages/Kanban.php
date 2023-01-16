f15ad4bc57e5e3f81939c48cb513249ecdf82024
EL OUFIR Hatim <eloufirhatim@gmail.com>
2023-01-16 15:00:47 +0100

    Optimize Scrum / Kanban components

--- app/Filament/Pages/Kanban.php
+++ app/Filament/Pages/Kanban.php
@@ -2,28 +2,18 @@
 
 namespace App\Filament\Pages;
 
+use App\Helpers\KanbanScrumHelper;
 use App\Models\Project;
-use App\Models\Ticket;
-use App\Models\TicketPriority;
-use App\Models\TicketStatus;
-use App\Models\TicketType;
-use App\Models\User;
 use Filament\Facades\Filament;
-use Filament\Forms\Components\Grid;
-use Filament\Forms\Components\Placeholder;
-use Filament\Forms\Components\Select;
-use Filament\Forms\Components\Toggle;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
 use Filament\Pages\Actions\Action;
 use Filament\Pages\Page;
 use Illuminate\Contracts\Support\Htmlable;
-use Illuminate\Support\Collection;
-use Illuminate\Support\HtmlString;
 
 class Kanban extends Page implements HasForms
 {
-    use InteractsWithForms;
+    use InteractsWithForms, KanbanScrumHelper;
 
     protected static ?string $navigationIcon = 'heroicon-o-view-boards';
 
@@ -32,17 +22,6 @@ class Kanban extends Page implements HasForms
     protected static string $view = 'filament.pages.kanban';
 
     protected static bool $shouldRegisterNavigation = false;
-
-    public bool $sortable = true;
-
-    public Project|null $project = null;
-
-    public $users = [];
-    public $types = [];
-    public $priorities = [];
-    public $includeNotAffectedTickets = false;
-
-    public bool $ticket = false;
 
     protected $listeners = [
         'recordUpdated',
@@ -64,53 +43,6 @@ class Kanban extends Page implements HasForms
         $this->form->fill();
     }
 
-    protected function getFormSchema(): array
-    {
-        return [
-            Grid::make([
-                'default' => 2,
-                'md' => 6
-            ])
-                ->schema([
-                    Select::make('users')
-                        ->label(__('Owners / Responsibles'))
-                        ->multiple()
-                        ->options(User::all()->pluck('name', 'id')),
-
-                    Select::make('types')
-                        ->label(__('Ticket types'))
-                        ->multiple()
-                        ->options(TicketType::all()->pluck('name', 'id')),
-
-                    Select::make('priorities')
-                        ->label(__('Ticket priorities'))
-                        ->multiple()
-                        ->options(TicketPriority::all()->pluck('name', 'id')),
-
-                    Toggle::make('includeNotAffectedTickets')
-                        ->label(__('Include not affected tickets'))
-                        ->columnSpan(2),
-
-                    Placeholder::make('search')
-                        ->label(new HtmlString('&nbsp;'))
-                        ->content(new HtmlString('
-                            <button type="button"
-                                    wire:click="filter" wire:loading.attr="disabled"
-                                    class="bg-primary-500 px-3 py-2 text-white rounded hover:bg-primary-600
-                                    disabled:bg-primary-300">
-                                ' . __('Filter') . '
-                            </button>
-                            <button type="button"
-                                    wire:click="resetFilters" wire:loading.attr="disabled"
-                                    class="ml-2 bg-gray-800 px-3 py-2 text-white rounded hover:bg-gray-900
-                                    disabled:bg-gray-300">
-                                ' . __('Reset filters') . '
-                            </button>
-                        ')),
-                ]),
-        ];
-    }
-
     protected function getActions(): array
     {
         return [
@@ -127,140 +59,12 @@ class Kanban extends Page implements HasForms
 
     protected function getHeading(): string|Htmlable
     {
-        $heading = '<div class="w-full flex flex-col gap-1">';
-        $heading .= '<a href="' . route('filament.pages.board') . '"
-                            class="text-primary-500 text-xs font-medium hover:underline">';
-        $heading .= __('Back to board');
-        $heading .= '</a>';
-        $heading .= '<div class="flex flex-col gap-1">';
-        $heading .= '<span>' . __('Kanban');
-        if ($this->project) {
-            $heading .= ' - ' . $this->project->name . '</span>';
-        } else {
-            $heading .= '</span><span class="text-xs text-gray-400">'
-                . __('Only default statuses are listed when no projects selected')
-                . '</span>';
-        }
-        $heading .= '</div>';
-        $heading .= '</div>';
-        return new HtmlString($heading);
+        return $this->kanbanHeading();
     }
 
-    public function getStatuses(): Collection
+    protected function getFormSchema(): array
     {
-        $query = TicketStatus::query();
-        if ($this->project && $this->project->status_type === 'custom') {
-            $query->where('project_id', $this->project->id);
-        } else {
-            $query->whereNull('project_id');
-        }
-        return $query->orderBy('order')
-            ->get()
-            ->map(function ($item) {
-                $query = Ticket::query();
-                if ($this->project) {
-                    $query->where('project_id', $this->project->id);
-                }
-                $query->where('status_id', $item->id);
-                return [
-                    'id' => $item->id,
-                    'title' => $item->name,
-                    'color' => $item->color,
-                    'size' => $query->count(),
-                    'add_ticket' => $item->is_default && auth()->user()->can('Create ticket')
-                ];
-            });
-    }
-
-    public function getRecords(): Collection
-    {
-        $query = Ticket::query();
-        $query->with(['project', 'owner', 'responsible', 'status', 'type', 'priority', 'epic']);
-        if ($this->project) {
-            $query->where('project_id', $this->project->id);
-        } else {
-            $query->whereHas('project', fn($query) => $query->where('status_type', 'default'));
-        }
-        if (sizeof($this->users)) {
-            $query->where(function ($query) {
-                return $query->whereIn('owner_id', $this->users)
-                    ->orWhereIn('responsible_id', $this->users);
-            });
-        }
-        if (sizeof($this->types)) {
-            $query->whereIn('type_id', $this->types);
-        }
-        if (sizeof($this->priorities)) {
-            $query->whereIn('priority_id', $this->priorities);
-        }
-        if ($this->includeNotAffectedTickets) {
-            $query->orWhereNull('responsible_id');
-        }
-        $query->where(function ($query) {
-            return $query->where('owner_id', auth()->user()->id)
-                ->orWhere('responsible_id', auth()->user()->id)
-                ->orWhereHas('project', function ($query) {
-                    return $query->where('owner_id', auth()->user()->id)
-                        ->orWhereHas('users', function ($query) {
-                            return $query->where('users.id', auth()->user()->id);
-                        });
-                });
-        });
-        return $query->get()
-            ->map(fn(Ticket $item) => [
-                'id' => $item->id,
-                'code' => $item->code,
-                'title' => $item->name,
-                'owner' => $item->owner,
-                'type' => $item->type,
-                'responsible' => $item->responsible,
-                'project' => $item->project,
-                'status' => $item->status->id,
-                'priority' => $item->priority,
-                'epic' => $item->epic,
-                'relations' => $item->relations,
-                'totalLoggedHours' => $item->totalLoggedSeconds ? $item->totalLoggedHours : null
-            ]);
-    }
-
-    public function recordUpdated(int $record, int $newIndex, int $newStatus): void
-    {
-        $ticket = Ticket::find($record);
-        if ($ticket) {
-            $ticket->order = $newIndex;
-            $ticket->status_id = $newStatus;
-            $ticket->save();
-            Filament::notify('success', __('Ticket updated'));
-        }
-    }
-
-    public function isMultiProject(): bool
-    {
-        return $this->project === null;
-    }
-
-    public function filter(): void
-    {
-        $this->getRecords();
-    }
-
-    public function resetFilters(): void
-    {
-        $this->form->fill();
-        $this->filter();
-    }
-
-    public function createTicket(): void
-    {
-        $this->ticket = true;
-    }
-
-    public function closeTicketDialog(bool $refresh): void
-    {
-        $this->ticket = false;
-        if ($refresh) {
-            $this->filter();
-        }
+        return $this->formSchema();
     }
 
 }
